fb76167a4c0190ec12234372b1b1730619279fdb
SHEIKH MD ANAMUL HAQUE <162632201+smanamulhaque@users.noreply.github.com>
2026-03-03 10:38:05 +0600

    Add files via upload

---
+++ Python MyProject/Basic_python/Loop/Break & Continue.py
@@ -0,0 +1,9 @@
+# For loop
+
+AnamulList1=["anamul","Haque","Rayhan","Shown","Awki","dsa"]
+for i in range(len(AnamulList1)):
+# for i in AnamulList1:
+    if i==4:
+        break
+        # continue
+    print(i)
