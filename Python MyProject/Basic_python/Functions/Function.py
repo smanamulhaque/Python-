ca17e0ebd15916aa1f2726d4ce3f97727770bccf
SHEIKH MD ANAMUL HAQUE <162632201+smanamulhaque@users.noreply.github.com>
2026-03-03 10:37:36 +0600

    Add files via upload

---
+++ Python MyProject/Basic_python/Functions/Function.py
@@ -0,0 +1,23 @@
+def AnamulFun (a,b):
+    sum=a+b
+    print(sum)
+def Minus(x,y):
+    sum = x - y
+    print(sum)
+
+def Multiply(c,d):
+    sum = c*d
+    print(sum)
+
+AnamulFun(10,20)
+Minus(20,10)
+Multiply(2,4)
+
+
+def my_function(name="Anamul" ):
+  print("Hello", name)
+
+my_function("Emil")
+my_function("Tobias")
+my_function()
+my_function("Linus")
---
+++ Python MyProject/Basic_python/Functions/ZIP Function.py
@@ -0,0 +1,5 @@
+from zipimport import ZIP64_EXTRA_TAG
+
+List1 = ["Anamul","Shown","Rayhan"]
+List2 = ["Google","MS","Nasa"]
+print(list(zip(List1,List2))) 
